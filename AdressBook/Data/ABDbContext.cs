e1977b2419306828830b4b7481b012bd88e818de
anxhela <anxhela@ANXHELA-OFFICE>
2023-02-03 09:53:20 +0100

    Add project files.

---
+++ AdressBook/Data/ABDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AdressBook.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdressBook.Data
+{
+    public class ABDbContext : DbContext
+    {
+        public ABDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
